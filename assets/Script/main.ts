7514403dc4758aabda7f75e01550d547d3fbbbd3
EThan <sun591854735@qq.com>
2023-12-11 17:01:33 +0800

    日语,女性

--- assets/Script/main.ts
+++ assets/Script/main.ts
@@ -59,7 +59,7 @@ export default class Main extends cc.Component {
   contentAnim: cc.Animation = null;
   canClick: boolean = false;
   _isLandscape: boolean = false;
-  _targetIndex: number[] = [0, 5, 3];
+  _targetIndex: number[] = [0, 4, 0];
 
   _titleList: string[] = ["スタイル選択", "サブジェクト選択", "ポーズ選択"];
 
